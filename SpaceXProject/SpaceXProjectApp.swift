300a775a3d542bef4454a94f9d007a970a70add6
jankubin <101505317+jankubin@users.noreply.github.com>
2023-07-28 10:06:50 +0200

    Initial Commit

---
+++ SpaceXProject/SpaceXProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXProjectApp.swift
+//  SpaceXProject
+//
+//  Created by Jan Kubín on 28.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
